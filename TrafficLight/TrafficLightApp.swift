be9b7d95ba9a377470f30183e5f903aa904896f4
Daniil Chupin <danielchupin@yandex.ru>
2023-07-19 11:42:30 +0700

    Initial Commit

---
+++ TrafficLight/TrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightApp.swift
+//  TrafficLight
+//
+//  Created by Даниил Чупин on 19.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
