db800ef5ea2150a88ad06f068a42dc91c005c7fd
Daniil Chupin <danielchupin@yandex.ru>
2023-07-19 12:39:11 +0700

    HW is done

--- TrafficLight/ContentView.swift
+++ TrafficLight/ContentView.swift
@@ -7,15 +7,71 @@
 
 import SwiftUI
 
-struct ContentView: View {
+enum TrafficLightState {
+    case none
+    case red
+    case yellow
+    case green
+}
+
+struct SignalView: View {
+    var color: Color
+    var lineWidth = 4.0
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        Circle()
+            .frame(width: 120, height: 120)
+            .foregroundColor(color)
+            .overlay(
+                Circle()
+                    .stroke(Color.black.opacity(0.7), lineWidth: lineWidth)
+            )
+    }
+}
+
+struct TrafficLightView: View {
+    @State private var lightState: TrafficLightState = .none
+    @State private var buttonText = "START"
+
+    var body: some View {
+        VStack(spacing: 30) {
+            SignalView(color: lightState == .red ? .red : .red.opacity(0.3))
+            
+            SignalView(color: lightState == .yellow ? .yellow : .yellow.opacity(0.3))
+            
+            SignalView(color: lightState == .green ? .green : .green.opacity(0.3))
+            
+            Spacer()
+            
+            Button(action: {
+                switch lightState {
+                case .none:
+                    lightState = .red
+                    buttonText = "NEXT"
+                case .red:
+                    lightState = .yellow
+                case .yellow:
+                    lightState = .green
+                case .green:
+                    lightState = .red
+                }
+            }) {
+                Text(buttonText)
+                    .font(.title)
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+            .padding(.bottom, 40)
         }
         .padding()
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        TrafficLightView()
     }
 }
 
